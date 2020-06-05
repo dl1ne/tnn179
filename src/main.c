4cb15ac600589f5cd110bbaefa59a4265889538a
dl1ne <dl1ne@darc.de>
2020-06-05 13:22:17 +0200

    dritter fix fuer version string

--- src/main.c
+++ src/main.c
@@ -413,11 +413,12 @@ static void mainf(void)
           "This version is compiled for %d Ports. "
           "Maximum %d L2-Links and %d Circuits.\r"
 #ifdef PPCONVERS
-          "conversd %5.5s-pp (c) dc6iq/dk5sg, TNN-Version by DL1XAO\r%s\r%s\r",
-          L2PNUM, LINKNMBR, NUMCIR, strchr(REV, ':')+2,author,autmod);
+          "conversd %5.5s-pp (c) dc6iq/dk5sg, TNN-Version by DL1XAO\r%s",
+          L2PNUM, LINKNMBR, NUMCIR, strchr(REV, ':')+2,author);
 #else
           "\r", L2PNUM, LINKNMBR, NUMCIR);
 #endif
+  hprintf("%s\r", autmod);
 
 #ifdef AXIPR_UDP
   /* Sperre setzen. */
